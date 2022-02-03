204e9e046958128fbe12d7ac5f326699b28c0a1f
Gottfried Akuffo <gottfried.akuffo@gmail.com>
2022-02-03 16:28:52 +0000

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,91 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-tsparticles";
 import Navbar from "./components/Navbar";
-
+import Header from "./components/Header";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        id="tsparticles"
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.5,
+                size: 20,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: false,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 50,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "line",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
